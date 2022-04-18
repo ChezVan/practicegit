ac9f6c58bafacd66fbe26a49a11a0d5b12bb87b3
ChezVan <chezney@thedigitalacademy.co.za>
2022-04-18 23:11:29 +0200

    add name

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>Git and React dont need local host 3000</h1>
+        <h1>Chezney  test</h1>
+       
       </header>
     </div>
   );
